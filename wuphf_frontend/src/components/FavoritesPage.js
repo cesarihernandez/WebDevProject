6ccf674b71686e8717c5e1a5df7673da8fed7b8a
Ryon Sajnovsky <zillionoo@Skyrs-MacBook-Air.local>
2023-08-10 18:59:56 -0400

    Added Favorites Page and styling.

---
+++ wuphf_frontend/src/components/FavoritesPage.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import FavoriteDataService from '../services/favorites';
+import DogDataService from '../services/dogs';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import { Link } from "react-router-dom";
+
+import "./FavoritesPage.css";
+
+const FavoritesPage = ({ user }) => {
+  const [favorites, setFavorites] = useState([]);
+  const [dogDetails, setDogDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      if (user) {
+        try {
+          const userId = user.googleId;
+          const response = await FavoriteDataService.getAll(userId);
+          console.log('Full response:', response);
+          console.log('Fetched favorites:', response.data);
+          setFavorites(response.data.favorites);
+        } catch (error) {
+          console.error('Error fetching favorites:', error);
+        }
+      }
+    };
+    fetchFavorites();
+  }, [user]);
+
+  useEffect(() => {
+    const fetchDogDetails = async () => {
+      const detailedDogDetails = await Promise.all(favorites.map(async (dogId) => {
+        try {
+          const response = await DogDataService.get(dogId);
+          return response.data;
+        } catch (error) {
+          console.error('Error fetching dog details:', error);
+          return null;
+        }
+      }));
+      setDogDetails(detailedDogDetails.filter(dog => dog !== null));
+    };
+    
+    if (favorites.length > 0) {
+      fetchDogDetails();
+    }
+  }, [favorites]);
+
+  return (
+    <div className="favorites-container">
+      <h1>Favorites Page</h1>
+      <div className="dogsList">
+        {dogDetails.map((dog) => (
+          <Col key={dog._id}>
+            <Card className="dogsListCard">
+              <Card.Img
+                className="smallPoster"
+                src={dog.poster}
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = "/images/dog-placeholder.png";
+                }}
+              />
+              <Card.Body>
+                <Card.Title className="cardTitle"> {dog.dog_breed}</Card.Title>
+                <Card.Text className="cardTitle">
+                  Dog Size: {dog.size}
+                </Card.Text>
+                <Card.Text>{dog.shedding}</Card.Text>
+                <Link to={"/dogs/" + dog._id}>View Reviews</Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
